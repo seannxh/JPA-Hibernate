5895f6ee3ff835261b982b05c8120486209d7c61
seannxh <nohsean6@gmail.com>
2025-04-24 01:52:29 -0500

    created and connected h2 table

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+create table course(
+
+    id bigint not null,
+    name varchar(255) not null,
+    author varchar(255) not null,
+    primary key (id)
+);
+--
+--JDBC a lot of sql codes and java
+--Spring JDBC same amount of sql but not as much as java
